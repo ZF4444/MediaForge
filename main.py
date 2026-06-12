616530f24f24f38f47ff7447199cf6ea65ea7d6d
ZF4444 <38451793+ZF4444@users.noreply.github.com>
2026-06-12 13:26:59 +0800

    main.py 支持接受命令行参数

--- main.py
+++ main.py
@@ -8391,5 +8391,9 @@ app.include_router(access_control_router.router)
 
 
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=3000)
+    import argparse, uvicorn
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--host", default="127.0.0.1")
+    parser.add_argument("--port", type=int, default=3000)
+    args = parser.parse_args()
+    uvicorn.run(app, host=args.host, port=args.port)
